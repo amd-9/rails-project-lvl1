585fd9852e2dcc047cf1d3c30cfc7cf09b6d4006
Anton Samoshin <asamoshin9@gmail.com>
2021-06-29 21:16:39 +0300

    Add autoload for form

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-require 'form'
-
 module HexletCode
   class Error < StandardError; end
   # Your code goes here...
 
   autoload(:VERSION, 'hexlet_code/version')
   autoload(:Tag, 'tag')
+  autoload(:Form, 'form')
 
   def self.form_for(entity, url: '#', &block)
     form = Form.new(entity, url)
